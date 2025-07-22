4220af75ecaf7bf676f0b0982b1a73cdce2d6d4e
likitha <likkilikitha214@gmail.com>
2025-07-22 13:43:37 +0530

    Add files via upload

---
+++ agepgm.py
@@ -0,0 +1,6 @@
+age=int(input("your current age"))
+years_left=90-age
+days_left=years_left*365
+months_left=years_left*12
+weeks_left=years_left*52
+print(f"your days_left:{days_left},your months left:{months_left} and your weeks_left:{weeks_left}")
