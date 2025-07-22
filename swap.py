3cd4677f3b98fe90729af5214cf9f22ee8bbb57f
likitha <likkilikitha214@gmail.com>
2025-07-22 10:23:51 +0530

    Add files via upload

---
+++ swap.py
@@ -0,0 +1,14 @@
+a=input("Enter a number")
+b=input("Enter a number")
+temp=a
+a=b
+b=temp
+print("a:"+a)
+print("b:"+b)
+a = input("Enter a number: ")
+b = input("Enter a number: ")
+# Swap values
+a, b = b, a
+print("a: " + a)
+print("b: " + b)
+
