81b754445fd2954427f7c2b41690af21c8c19385
likitha <likkilikitha214@gmail.com>
2025-07-22 10:24:53 +0530

    Add files via upload

---
+++ typecasting.py
@@ -0,0 +1,13 @@
+name="likitha"
+l=(len(name))
+print("Your name has" +" "+ str(l) + " " +"characters")
+new_length=str(l)
+print(type(l))
+print(type(new_length))
+print(10+10)
+print("10"+"10")
+print(int("10")+int("10"))
+print(10+float(10))
+name="likitha"
+print(10+name)
+print(10+int(name))
